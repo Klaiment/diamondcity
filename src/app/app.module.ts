d695d2ca5d014258e34fb3cbc5332b18268f11de
Klaiment <clement62ing@gmail.com>
2024-01-18 02:51:36 +0100

    visa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
@@ -13,6 +13,15 @@ import { SecretaireComponent } from './sidebar/secretaire/secretaire.component';
 import { InfosComponent } from './sidebar/infos/infos.component';
 import { AccoutingComponent } from './sidebar/accouting/accouting.component';
 import { LogoutComponent } from './logout/logout.component';
+import { VisaComponent } from './visa/visa.component';
+import {CommonModule} from "@angular/common";
+import {ToolbarModule} from "@syncfusion/ej2-angular-navigations";
+import {GridAllModule} from "@syncfusion/ej2-angular-grids";
+import {NumericTextBoxAllModule, RatingAllModule} from "@syncfusion/ej2-angular-inputs";
+import {DialogModule} from "@syncfusion/ej2-angular-popups";
+import {DatePickerAllModule} from "@syncfusion/ej2-angular-calendars";
+import {DropDownListAllModule} from "@syncfusion/ej2-angular-dropdowns";
+import {CheckBoxModule} from "@syncfusion/ej2-angular-buttons";
 
 
 
@@ -26,14 +35,27 @@ import { LogoutComponent } from './logout/logout.component';
     SecretaireComponent,
     InfosComponent,
     AccoutingComponent,
-    LogoutComponent
+    LogoutComponent,
+    VisaComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
+    CommonModule,
+    ToolbarModule,
+    GridAllModule,
+    NumericTextBoxAllModule,
+    RatingAllModule ,
+    DialogModule,
+    DatePickerAllModule,
+    DropDownListAllModule,
+    ReactiveFormsModule,
+    FormsModule,
+    CheckBoxModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { registerLicense } from '@syncfusion/ej2-base';
 
 import { AppModule } from './app/app.module';
 
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NAaF5cWWJCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdnWX5fd3RSR2VeWUVxXUs=');
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
